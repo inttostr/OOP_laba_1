cabea529884fdd0571a5b8c9e475d5e4e7dd1bfe
inttostr <ike1ke@yandex.ru>
2024-04-26 20:17:34 +0300

    laba2

---
+++ laba2/laba2_sync_method/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Diagnostics;
+using System.Threading;
+class Program1
+{
+    static void Main(string[] args)
+    {
+        Stopwatch stopwatch = new Stopwatch();
+        var client = new HttpClient();
+        client.DefaultRequestHeaders.Add("User-Agent", "MyApp");
+
+        // Запрос вакансий по компании "МГТУ СТАНКИН"
+        HttpResponseMessage response = client.GetAsync("https://api.hh.ru/vacancies?employer_id=3094855").Result;
+        string json = response.Content.ReadAsStringAsync().Result;
+        File.WriteAllText("vacancies_mgtu_stankin_sync.json", json); // Сохраняем ответ в файл
+        Console.WriteLine("Ответ от API hh.ru по запросу 'МГТУ СТАНКИН' сохранен в файл vacancies_mgtu_stankin_sync.json");
+Thread.Sleep(5000);
+stopwatch.Stop();
+        Console.WriteLine($"Время выполнения: {stopwatch.ElapsedMilliseconds} мс");
+        //("Общее время работы: " + DateTime.Now.TimeOfDay);
+    }
+}
